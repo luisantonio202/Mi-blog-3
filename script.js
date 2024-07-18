fd8462b77fe300be4963f3e422b51e997d208967
luisantonio202 <162599487+luisantonio202@users.noreply.github.com>
2024-07-17 21:55:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.getElementById('downloadBtn').addEventListener('click', function () {
-    const link = document.createElement('a');
-    link.href = '/Ev#3_Simbologia.pdf'; // Reemplaza con la ruta real de tu archivo
-    link.download = 'Ev#3_Simbologia.pdf'; // Reemplaza con el nombre real del archivo
-    document.body.appendChild(link); // Añade el enlace al DOM
-    link.click(); // Simula el clic en el enlace
-    document.body.removeChild(link); // Remueve el enlace del DOM
-});
+
