c1a28d8a141a1ff494cc4502facd059acd32c1ac
luisantonio202 <162599487+luisantonio202@users.noreply.github.com>
2024-08-18 21:34:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+document.getElementById("boton-inicio").addEventListener("click", function() {
+    window.location.href = "https://luisantonio202.github.io/Mi-Blog/";
+});
+
+document.getElementById("boton-notas").addEventListener("click", function() {
+  window.location.href = "https://luisantonio202.github.io/Mi-Blog-2/";
+});
+
+document.getElementById("boton-proyectos").addEventListener("click", function() {
+  window.location.href = "https://luisantonio202.github.io/Mi-Blog-3/";
+});
